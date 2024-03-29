14c4f37c6dddd7f7917998164ecbe65c98d15468
anan <ananahmed550@gmail.com>
2024-03-29 02:59:14 +0200

    init push

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "anan.bucket"         # S3 bucket where the Terraform state file is stored
+    key            = "terraform.tfstate"   # Key (path) within the bucket to store the state file
+    region         = "us-east-1"           # AWS region of the S3 bucket
+    dynamodb_table = "anan.table"     # Name of the DynamoDB table for state locking
+  }
+}
+
